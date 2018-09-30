54aadb3d4503809a272ab33ee98005c589755f4d
idindrakusuma <indrakusuma.udinus@gmail.com>
2018-09-30 22:21:23 +0700

    add mongoDB connection and setup

---
+++ server/datasources.development.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  db: {
+    name: 'db',
+    connector: 'mongodb',
+    url: process.env.DB_URL,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+};

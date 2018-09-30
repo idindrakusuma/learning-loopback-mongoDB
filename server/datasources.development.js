f8137f4198fc880db497c022460c26473b49ed8b
idindrakusuma <indrakusuma.udinus@gmail.com>
2018-09-30 22:45:00 +0700

    fix issue loopback model cant created because db connector not detected

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -1,12 +1,8 @@
 'use strict';
 require('dotenv').config();
 
-console.log(process.env.DB_NAME);
-
 module.exports = {
   db: {
-    name: 'db',
-    connector: 'mongodb',
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
